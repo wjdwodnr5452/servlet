e37cdb9a659e43abed87cd686a0b3f2258620da0
정재욱 <wjdwodnr5452@naver.com>
2024-02-25 23:50:14 +0900

    유연한 컨트롤러1 - v5

---
+++ src/main/java/hello/servlet/web/frontcontroller/v5/FrontControllerServletV5.java
@@ -0,0 +1,25 @@
+package hello.servlet.web.frontcontroller.v5;
+
+import hello.servlet.web.frontcontroller.v3.controller.MemberFormControllerV3;
+import hello.servlet.web.frontcontroller.v3.controller.MemberListControllerV3;
+import hello.servlet.web.frontcontroller.v3.controller.MemberSaveControllerV3;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@WebServlet(name = "frontControllerServletV5", urlPatterns = "/front-controller/v5/*")
+public class FrontControllerServletV5 extends HttpServlet {
+
+    private final Map<String, Object> handlerMappingMap = new HashMap<>();
+    private final List<MyHandlerAdapter> handlerAdapters = new ArrayList<>();
+
+    public FrontControllerServletV5() {
+        handlerMappingMap.put("/front-controller/v3/members/new-form", new MemberFormControllerV3());
+        handlerMappingMap.put("/front-controller/v3/members/save", new MemberSaveControllerV3());
+        handlerMappingMap.put("/front-controller/v3/members", new MemberListControllerV3());
+    }
+}
